dc9b2c6424efbc72cc26b8cf47ad43ae78f7d1c7
angelslanding <angelsown4056@yahoo.com>
2017-08-15 22:47:56 -0700

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 
 var {Todo} = require('./models/todo');
@@ -29,6 +31,26 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return res.status(404).send();
+  }
+  res.send({todo});
+  console.log(JSON.stringify(todo, undefined, 2));
+  }, (e) => {
+    console.log(e);
+    res.status(400).send(e);
+  });
+
 });
 
 app.listen(3000, () => {
