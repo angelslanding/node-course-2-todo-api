40c12938cdb53d3e4be78d930e0bfb173991667b
angelslanding <angelsown4056@yahoo.com>
2017-08-16 15:03:42 -0700

    Test the DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -42,7 +42,10 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove({_id: id}).then((todo) => {
-    return res.send(todo);
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
